336f6e4c5af53db65592d45b712fa1fd81a4f601
martinGM05 <mtn.gm05@gmail.com>
2021-06-09 17:29:33 -0500

    Singleton patter with Provider

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class TabsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
-   );
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+   ),
+    );
   }
 }
 
 class _Navegacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Para ti'),
         BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
@@ -30,16 +38,28 @@ class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      physics: BouncingScrollPhysics(),
+      //physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
         Container(
           color: Colors.red,
         ),
-
         Container(
           color: Colors.green,
         )
       ],
     );
   }
+}
+
+class _NavegacionModel with ChangeNotifier{
+  int _paginaActual = 0;
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual(int valor){
+    this._paginaActual = valor;
+    notifyListeners();
+  }
+
 }
